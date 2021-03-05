32eabe061f54b06bfa3ee131de36e33666e7e941
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-05 14:51:26 +0200

    Fixed bug with the installation

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo Marking PhoneInformationTracer.py as executable...
 chmod +x PhoneInformationTracer.py
 
 echo Creating Symlink...
-ln -s PhoneInformationTracer.py /sbin/phoneInformationTracer
+ln -s $(pwd)/PhoneInformationTracer.py /sbin/phoneInformationTracer
 
 echo Everything\'s set up! Run phoneInformationTracer to get started!
