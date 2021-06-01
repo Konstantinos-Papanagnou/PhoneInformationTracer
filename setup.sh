28962719ca855d3d18357369d5ae969f1f9d950d
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-06-01 13:54:33 +0300

    Change installation script to install on bin instead of sbin

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo Marking PhoneInformationTracer.py as executable...
 chmod +x PhoneInformationTracer.py
 
 echo Creating Symlink...
-ln -s $(pwd)/PhoneInformationTracer.py /sbin/phoneInformationTracer
+ln -s $(pwd)/PhoneInformationTracer.py /bin/phoneInformationTracer
 
 echo Everything\'s set up! Run phoneInformationTracer to get started!
